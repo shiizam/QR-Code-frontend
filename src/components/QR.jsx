5ba6a0c42a30da756432386e145495b3a465d9f8
shiizam <selftaughtcoder21@gmail.com>
2023-02-23 16:47:02 -0500

    Add website to qr code 'alt'

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './style.css?inline';
+import './style.css';
 import qrImage from '../assets/image-qr-code.png';
 
 export const QR = () => {
@@ -8,8 +8,8 @@ export const QR = () => {
 				<img className='panel-qr-img' src={qrImage} alt="qr" />
 				<h3 className='panel-title'>Improve your front-end skills by building projects</h3>
 				<p className='panel-body'>
-				Scan the QR code to visit Frontend Mentor and take your coding skills to
-				the next level
+					Scan the QR code to visit Frontend Mentor and take your coding skills to
+					the next level
 				</p>
 		</div>
 	)
