4c8391f979f721baedbae7a8ea3f3a17b194c0da
shiizam <selftaughtcoder21@gmail.com>
2023-02-23 12:36:23 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './components/style.css'
+import { QR } from './components/QR'
+
+function App() {
+  return(
+    <QR />
+  )
+}
+
+export default App
---
+++ src/components/QR.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './style.css?inline';
+import qrImage from '../assets/image-qr-code.png';
+
+export const QR = () => {
+	return (
+		<div className='panel'>
+				<img className='panel-qr-img' src={qrImage} alt="qr" />
+				<h3 className='panel-title'>Improve your front-end skills by building projects</h3>
+				<p className='panel-body'>
+				Scan the QR code to visit Frontend Mentor and take your coding skills to
+				the next level
+				</p>
+		</div>
+	)
+}
